716a6b2a876d295d0745ea54dce20fdf4fdf98e8
vijaydehraj <vijaydehraj15@gmail.com>
2023-04-24 14:47:47 +0530

    added a blank line after method

--- src/main/java/com/servlet/AddServlet.java
+++ src/main/java/com/servlet/AddServlet.java
@@ -2,11 +2,13 @@ package com.servlet;
 
 import java.io.IOException;
 
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 
 public class AddServlet extends HttpServlet {
 
